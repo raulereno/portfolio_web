1347c02c14cba88fbd2c637d49b7b28929fcd9ae
raulereno <raulereno@gmail.com>
2022-10-30 12:47:15 -0300

    portfolio v0.1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,26 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import { NavBarComponent } from './components/header/nav-bar/nav-bar.component';
+import { MainComponent } from './components/main/main.component';
+import { HeaderComponent } from './components/header/header.component';
+import { HomeComponent } from './components/home/home.component';
+import { AboutComponent } from './components/main/about/about.component';
+import { ResumeComponent } from './components/main/resume/resume.component';
+import { PortfolioComponent } from './components/main/portfolio/portfolio.component';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    NavBarComponent,
+    MainComponent,
+    HomeComponent,
+    AboutComponent,
+    ResumeComponent,
+    PortfolioComponent,
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  imports: [BrowserModule, AppRoutingModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
